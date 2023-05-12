876edd46b3cf321ada792447472cae1b08e6b152
Glory Stephen Essien <sglory762@gmail.com>
2023-05-12 00:15:43 -0700

    JS added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const changeButton = document.getElementById("change-button");
+const learningStatus = document.getElementById("learning-status");
+
+
+changeButton.addEventListener("click", function() {
+
+  learningStatus.textContent = "I am currently learning JavaScript";
+});
